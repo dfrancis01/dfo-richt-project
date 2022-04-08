e7fb635222d3975c462f20349207a5b9efa15df4
Francis <13628072+dfrancis01@users.noreply.github.com>
2022-04-08 11:33:08 -0400

    Add Parameters to specify host name & port number

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,15 @@
 
 #' Run Shiny App
+#' @author Dorlan Francis
+#'
+#' @param port numeric specify port number
+#' @param host character specify host name
 #'
 #' @export
 #'
 #' @importFrom shiny shinyApp
-run_app <- function(){
+run_app <- function(host=NULL, port = NULL){
+  if(!is.null(host)) options(shiny.host = as.character(host))
+  if(!is.null(port)) options(shiny.port = as.integer(port))
   shiny::shinyApp(ui = shinyUI, server = shinyServer)
 }
