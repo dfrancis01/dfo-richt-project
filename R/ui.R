8117c588dcd2bd7de8572af60f1f160da4426fdf
Francis <13628072+dfrancis01@users.noreply.github.com>
2022-04-13 13:49:28 -0400

    Fix R CMD warning about shinydashboardPlus overwriting functions in shinydashboard by removing it from @import in UI

--- R/ui.R
+++ R/ui.R
@@ -4,17 +4,17 @@
 #' @param session
 #'
 #' @import shiny
-#' @import shinydashboard
-#' @importFrom  shinydashboardPlus dashboardHeader dashboardSidebar dashboardPage dashboardFooter taskItem notificationItem messageItem
+#' @importFrom  shinydashboardPlus dashboardHeader dashboardControlbar dashboardSidebar dashboardPage dashboardFooter taskItem notificationItem messageItem
 #' @noRd
 #' @return Shiny UI
 
 shinyUI <- function(session){
     shinydashboardPlus::dashboardPage(
-        title = "RiTCH",
-        shinydashboardPlus::dashboardHeader(title = mod_Title_Logo_UI("ritchLogo")),
-        shinydashboardPlus::dashboardSidebar(),
-        shinydashboard::dashboardBody(
+        title = "RiTCH"
+        ,shinydashboardPlus::dashboardHeader(title = mod_Title_Logo_UI("ritchLogo"))
+        ,shinydashboardPlus::dashboardSidebar()
+        ,controlbar = shinydashboardPlus::dashboardControlbar()
+        ,shinydashboard::dashboardBody(
             tags$head(
               tags$link(rel = "icon", type="image/png", href="img/RiCHT_16x16px.png"),
               tags$link(rel = "stylesheet", type = "text/css", href = "css/RiTCH.css"))
