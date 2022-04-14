680109325823fdf26a1eb36d58d3ca8fdd679888
Francis <13628072+dfrancis01@users.noreply.github.com>
2022-04-14 17:05:33 -0400

    Fix install error added bsplus and fresh to imports

--- R/ui_utils.R
+++ R/ui_utils.R
@@ -2,9 +2,6 @@
 #'
 #' @description This utility function is where all html links made in the `<head>`  tag will be placed.
 #' @return HTML Header Tag with link tags for Favicon, CSS, Javascript
-#' @examples
-#' shinydashboard::dashboardBody(
-#'  headerLinks(), ...
 headerLinks <- function(){
   tags$head(
     tags$link(rel = "icon", type="image/png", href="img/RiCHT_16x16px.png"),
@@ -19,15 +16,15 @@ headerLinks <- function(){
 #' @return fresh theme
 #' @importFrom fresh use_theme create_theme adminlte_color adminlte_sidebar
 RiCHT_Theme <- function(){
-  use_theme(
-    create_theme( #Adminlte dashboard theme colour overide
+  fresh::use_theme(
+    fresh::create_theme( #Adminlte dashboard theme colour overide
       #https://rdrr.io/cran/fresh/man/adminlte_color.html
-      adminlte_color(
+      fresh::adminlte_color(
         light_blue = "#396B39",              #primary colour override
         aqua = "#004455"                     #info colour override
       ),
       #https://rdrr.io/cran/fresh/man/adminlte_sidebar.html
-      adminlte_sidebar(
+      fresh::adminlte_sidebar(
         dark_bg = "#004455",                 #sidebar background colour darkmod default
         dark_hover_bg = "#7D9FA8",           # " " " on hover
         dark_color = "#fff",                 #Sidebar Text Colour
