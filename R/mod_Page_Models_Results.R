9ade3d7038bb0577fe0184017a706e1ec74eaa41
dfrancis01 <13628072+dfrancis01@users.noreply.github.com>
2022-05-06 10:35:50 -0400

    10 models results screen (#32)
    * Resolves #10 Model Results Screen UI mockup
    
    * With issue #10
    -Added sweetalert message box for download report button
    -added TODO notes for further development

--- R/mod_Page_Models_Results.R
+++ R/mod_Page_Models_Results.R
@@ -37,15 +37,15 @@ mod_Page_Models_Results_UI <- function(id) {
         ,collapsible = TRUE
         ,headerBorder = TRUE
         ,align = "center"
-        # ,img(src="img/Placeholder.GGPlot.Opent.Street.Map.png", style ="max-height:150px;")#TODO:Placeholder Image for Decision Tree
-        ,plotOutput(ns("image")) #did not work
-        #,imageOutput(ns("image"), height = "150px") #did not work
+        #TODO:Placeholder Map Proxy
+        # ,plotOutput(ns("map"))
+        ,imageOutput(ns("map")) #Alternative Method
       )#Box
       ,column(width = 2, align = "right",
-        # shiny::actionButton(inputId = ns("downloadResults"), label = "Downlaod Results", icon = icon("file", lib = "font-awesome"), class = "btn btn-primary btn-lg btn-app-dark")
-      tags$button(id=ns("downloadResults"), class = "action-buttonshiny-bound-input btn btn-primary btn-lg ", "Download Results")
-        )
-      )#fluidRow
+        shiny::actionButton(inputId = ns("downloadResults"), label = "Downlaod Results", icon = icon("file", lib = "font-awesome"), class = "btn btn-primary btn-lg btn-app-dark")
+      )#column
+    )#fluidRow
+
 
     # MAVF Scores Table Section ----------------------------------------------------------
     ,fluidRow(
@@ -89,30 +89,44 @@ mod_Page_Models_Results_UI <- function(id) {
 #'
 #' @importFrom ggplot2 aes geom_bar ggplot
 #' @importFrom DT renderDT
+#' @importFrom shinyWidgets sendSweetAlert
 #'
 mod_Page_Models_Results_Server <- function(id) {
+  #TODO:Module Inputs {Proxy_Map, Search_Data}
   moduleServer(
     id,
     function(input, output, session) {
-      output$image <- renderImage({
-        # When input$n is 3, filename is ./images/image3.jpeg
+      ns <- session$ns #Needed if UI will be rendered server side
+      #Download Button Click Event
+      observeEvent(input$downloadResults, {
+        #TODO: Sweet Alert Placeholder
+        shinyWidgets::sendSweetAlert(
+          session = session,
+          title = "Information",
+          type = "info",
+          text = "Download Report coming soon."
+        )
+      })
+
+      #TODO: Temp placeholder for Proxy Map, map image
+      output$map <- renderImage({
         filename <- normalizePath(file.path('inst/www/img/Placeholder.GGPlot.Opent.Street.Map.png'))
-
-        height <- session$clientData$output_plot2_height
-
         # Return a list containing the filename and alt text
         list(src = filename,
-             height = 400,
+             height = 400,             #Note:Effective size of placeholder Map
+
              width = 'auto',
              alt = paste("Placeholder Openstreet Map"))
 
       }, deleteFile = FALSE)
 
-      #Placeholder Random DT Datatable from shinipsum
+      #TODO: Placeholder for search results Random DT Datatable from shinipsum
       output$data_table <- DT::renderDT({
         shinipsum::random_DT(10, 5)
       })
-      #Place Holder Horizontal Bar graph
+
+      #TODO: Placeholder Horizontal Bar graph ggplot test data
+
       output$horizontalBar <- renderPlot({
         g <- ggplot2::ggplot(ggplot2::mpg)
         g + ggplot2::geom_bar(ggplot2::aes(x = class, fill = factor(ggplot2::mpg$cyl))) + ggplot2::coord_flip()
