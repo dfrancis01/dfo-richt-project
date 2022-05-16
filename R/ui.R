4f73e45db97bd4de22e46c3415e4f951fb1184b6
dfrancis01 <13628072+dfrancis01@users.noreply.github.com>
2022-05-16 19:38:47 +0000

    Fix #50 Species Menu Badge dev to con - Updated Species menu badge from 'dev' in development to 'con' under construction.

--- R/ui.R
+++ R/ui.R
@@ -34,7 +34,7 @@ shinyUI <- function(session){
                 ,menuItem(strong("Primary Research"), tabName = "primaryResearch", icon = NULL, badgeLabel = "con", badgeColor = "yellow")
 
                 ,menuItem("Designatable Unit", tabName = "map", icon = icon("search-location", lib = "font-awesome"), badgeLabel = "dev", badgeColor = "red", selected = TRUE)
-                ,menuItem("Species", tabName = "species", icon = icon("fish", lib = "font-awesome"), badgeLabel = "dev", badgeColor = "red")
+                ,menuItem("Species", tabName = "species", icon = icon("fish", lib = "font-awesome"), badgeLabel = "con", badgeColor = "yellow")
                 ,menuItem("Aquatic Features", tabName = "habitat", icon = icon("water", lib = "font-awesome"), badgeLabel = "dev", badgeColor = "red")
                 ,menuItem("Primary Results", tabName = "primaryResults", icon = icon("file",lib = "font-awesome"), badgeLabel = "dev", badgeColor = "red")
 
